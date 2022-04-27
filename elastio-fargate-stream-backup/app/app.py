28150b1f74992d3a49fa250bcc5d3f2dd5799d58
ViacheslavSaievskyi <vsaievskyi@elastio.com>
2022-04-27 19:47:14 +0300

    delete loggs from stdout, stop using the arch pattern, hard-code FROM line

--- elastio-fargate-stream-backup/app/app.py
+++ elastio-fargate-stream-backup/app/app.py
@@ -8,11 +8,6 @@ logging.basicConfig(
 
 
 def backup(bucket_name, file_key, stream_name, vault):
-    logging.info("Stream backup from s3 will start.")
-    logging.info("Bucket name: %s" % bucket_name)
-    logging.info("File key: %s" % file_key)
-    logging.info("Stream name: %s" % stream_name)
-    logging.info("Vault name: %s" % vault)
     status = os.system(f"python3 s3reader.py --bucket_name {bucket_name} --file_key {file_key} | elastio stream backup --stream-name {stream_name} --vault {vault}")
     logging.info(f"Task status code: {status}")
 

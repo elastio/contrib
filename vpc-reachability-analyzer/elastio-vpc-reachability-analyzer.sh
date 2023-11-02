cebb5a51e4ccef9d614a67e8125ca6eb2c8e4cf4
arybitskyi <81738703+arybitskyi@users.noreply.github.com>
2023-11-02 10:43:26 +0200

    Remove spaces.

--- vpc-reachability-analyzer/elastio-vpc-reachability-analyzer.sh
+++ vpc-reachability-analyzer/elastio-vpc-reachability-analyzer.sh
@@ -8,30 +8,30 @@ do
     do
 	  instanceID=$(aws ec2 run-instances --image-id $ami --instance-type t2.nano --subnet-id $subnetID \
 	    --associate-public-ip-address --query "Instances[].InstanceId" --output text --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=elastio-reachability-analyzer}]')
-	  
+
 	  while [[ $(aws ec2 describe-instances --instance-ids $instanceID --query "Reservations[].Instances[].State.Name" --output text) != "running" ]]
 	  do
 	    sleep 5
 	  done
-	  
+
 	  pathID=$(aws ec2 create-network-insights-path --source $instanceID --protocol TCP --filter-at-source '{"DestinationAddress": "8.8.8.8"}' \
 	    --query "NetworkInsightsPath.NetworkInsightsPathId" --output text)
-	  
+
 	  analysisID=$(aws ec2 start-network-insights-analysis --network-insights-path-id $pathID --query "NetworkInsightsAnalysis.NetworkInsightsAnalysisId" --output text)
-	  
+
 	  while [[ $(aws ec2 describe-network-insights-analyses --network-insights-analysis-ids $analysisID --query "NetworkInsightsAnalyses[].Status" --output text) != "succeeded" ]]
 	  do
 	    sleep 5
 	  done
-	  
+
 	  analysisResult=$(aws ec2 describe-network-insights-analyses --network-insights-analysis-ids $analysisID --query "NetworkInsightsAnalyses[].NetworkPathFound" --output text)
-	  
+
 	  output=$(aws ec2 delete-network-insights-analysis --network-insights-analysis-id $analysisID)
-	  
+
 	  output=$(aws ec2 delete-network-insights-path --network-insights-path-id $pathID)
-	  
+
 	  output=$(aws ec2 terminate-instances --instance-ids $instanceID)
-	  
+
 	  echo " - $subnetID - Reachable: $analysisResult"
 	  echo
 	done
